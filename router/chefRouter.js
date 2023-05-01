8f36c2c9f65cdf4192f288200dbbf20e02842c2a
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-05-01 23:15:08 +0600

    Update chefRouter.js

--- router/chefRouter.js
+++ router/chefRouter.js
@@ -3,24 +3,24 @@ const chefs = require('../chefsData/chefdata.json')
 
 
 // Getting individual chef's by id;
-router.get('/chef/:id',(req,res)=>{
+router.get('/chef/:id', (req, res) => {
     const id = req.params.id
-    const singleChef = chefs.find(chef=>chef.id==id)
+    const singleChef = chefs.find(chef => chef.id == id)
     res.send(singleChef)
 })
 
 
 // Getting the all chef's;
-router.get('/all',(req,res)=>{
+router.get('/all', (req, res) => {
     res.send(chefs)
 })
 
 
 // Not found Routes;
-router.get('*',(req,res)=>{
+router.get('*', (req, res) => {
     res.send({
-        message:"404 Error ! Page Not found...",
-        admin:"Minhazul Abedin Munna"
+        message: "404 Error ! Page Not found...",
+        admin: "Minhazul Abedin Munna"
     })
 })
 
